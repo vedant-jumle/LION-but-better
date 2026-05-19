91ca98a7f08d2aa08f6e6823b7eee63c031d3843
VedantJumle <vedantjumle@gmail.com>
2026-05-19 17:00:02 +0200

    update training slrum script with correct cpu and mem config

--- img_classification/train_lion.sh
+++ img_classification/train_lion.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=gpu-v100
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-task=1
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=4G
 #SBATCH --account=Education-EEMCS-MSc-DSAIT
 #SBATCH --output=/scratch/vvjumle/logs/lion_train_%j.out
 #SBATCH --error=/scratch/vvjumle/logs/lion_train_%j.err
