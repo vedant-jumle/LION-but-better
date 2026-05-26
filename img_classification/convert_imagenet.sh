acf4aa2ad61f083fd2898de20a1a18a83194a3d6
VedantJumle <vedantjumle@gmail.com>
2026-05-26 18:48:27 +0200

    update convertion script with correct DelftBlue memory

--- img_classification/convert_imagenet.sh
+++ img_classification/convert_imagenet.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --account=Education-EEMCS-MSc-DSAIT
 #SBATCH --output=/scratch/vvjumle/logs/imagenet_convert_%j.out
 #SBATCH --error=/scratch/vvjumle/logs/imagenet_convert_%j.err
